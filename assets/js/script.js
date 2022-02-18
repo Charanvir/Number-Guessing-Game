bbc382ad6afc9078bc6e24f9d11eab445c3c5937
Charanvir <charanvir123@gmail.com>
2022-02-18 03:40:09 -0500

    initial HTML added and button added to start function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,35 @@
-let maximum = parseInt(prompt("Enter A Maximum Number"));
-while (!maximum) {
-    maximum = parseInt(prompt("Enter A Valid Number"));
-}
+const startButton = document.querySelector("button")
 
-
-
-const target = Math.floor(Math.random() * maximum) + 1;
-
-
-let guess = parseInt(prompt("Enter Your Guess"));
-let attempts = 1;
-
-
-while (parseInt(guess) !== target) {
-    if (guess === "quit") break;
-    attempts++;
-    if (guess > target) {
-        guess = prompt("Too High");
-    } else {
-        guess = prompt("Too Low")
+startButton.addEventListener("click", () => {
+    let maximum = parseInt(prompt("Enter A Maximum Number"));
+    while (!maximum) {
+        maximum = parseInt(prompt("Enter A Valid Number"));
     }
-}
-if (guess === "quit") {
-    prompt("You have Quit")
-} else {
-    prompt(`Congrats, you are Correct! It took you ${attempts} guesses`);
-}
+
+
+
+    const target = Math.floor(Math.random() * maximum) + 1;
+
+
+    let guess = parseInt(prompt("Enter Your Guess"));
+    let attempts = 1;
+
+
+    while (parseInt(guess) !== target) {
+        if (guess === "quit") break;
+        attempts++;
+        if (guess > target) {
+            guess = prompt("Too High");
+        } else {
+            guess = prompt("Too Low")
+        }
+    }
+    if (guess === "quit") {
+        prompt("You have Quit")
+    } else {
+        prompt(`Congrats, you are Correct! It took you ${attempts} guesses`);
+    }
+})
+
+
 
