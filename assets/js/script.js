5f207b39f37440b260a73d0dacc55775269fbd3f
Charanvir <charanvir123@gmail.com>
2022-02-19 02:44:11 -0500

    added a quitting paramter

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,13 @@ const resetButton = document.querySelector(".rButton");
 startButton.addEventListener("click", () => {
     const newText = game();
     h1.innerText = newText;
-    winningImage.src = "https://media.istockphoto.com/vectors/first-prize-gold-trophy-iconprize-gold-trophy-winner-first-prize-vector-id1183252990?k=20&m=1183252990&s=612x612&w=0&h=BNbDi4XxEy8rYBRhxDl3c_bFyALnUUcsKDEB5EfW2TY=";
+    if (newText === "You have quit the game") {
+        winningImage.src = "https://i0.wp.com/positivesharing.com/wp-content/uploads/2008/04/quit.jpg?w=474&ssl=1"
+        winningImage.alt = "QUIT"
+    } else {
+        winningImage.src = "https://media.istockphoto.com/vectors/first-prize-gold-trophy-iconprize-gold-trophy-winner-first-prize-vector-id1183252990?k=20&m=1183252990&s=612x612&w=0&h=BNbDi4XxEy8rYBRhxDl3c_bFyALnUUcsKDEB5EfW2TY=";
+        winningImage.alt = "image of a trophy to celebrate you winning the game"
+    }
     startButton.disabled = true;
 })
 
@@ -43,7 +49,7 @@ const game = () => {
         }
     }
     if (guess === "quit") {
-        prompt("You have Quit")
+        return 'You have quit the game'
     } else {
         return `Congrats, you are Correct! It took you ${attempts} guesses`;
     }
