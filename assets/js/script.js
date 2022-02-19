5d121e3cae11a8e9c79340bd9202991a11a2bf2a
Charanvir <charanvir123@gmail.com>
2022-02-19 01:59:45 -0500

    reset button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
-const startButton = document.querySelector("button")
-const h1 = document.querySelector('h1')
-winningImage = document.querySelector("img")
+const startButton = document.querySelector(".sButton");
+const h1 = document.querySelector('h1');
+winningImage = document.querySelector("img");
+const resetButton = document.querySelector(".rButton");
 
 
 startButton.addEventListener("click", () => {
     const newText = game();
     h1.innerText = newText;
     winningImage.src = "https://media.istockphoto.com/vectors/first-prize-gold-trophy-iconprize-gold-trophy-winner-first-prize-vector-id1183252990?k=20&m=1183252990&s=612x612&w=0&h=BNbDi4XxEy8rYBRhxDl3c_bFyALnUUcsKDEB5EfW2TY=";
+    startButton.disabled = true;
+})
+
+resetButton.addEventListener("click", () => {
+    const resetText = reset();
+    h1.innerText = resetText;
+    winningImage.src = "https://media.mixbook.com/images/templates/97_1_0_m.jpg";
+    startButton.disabled = false;
 })
 
 const game = () => {
@@ -38,6 +47,10 @@ const game = () => {
     } else {
         return `Congrats, you are Correct! It took you ${attempts} guesses`;
     }
+}
+
+const reset = () => {
+    return "Number Guessing Game!";
 }
 
 
